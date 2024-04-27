a55d8ecc8ec3bf914914916e14e23b0725dd0e70
Arjun Bhandari <arjun12345bhandari@gmail.com>
2024-04-27 23:05:57 +0530

    All Posts fixed

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,76 +1,71 @@
-import React, { useEffect, useState } from 'react'
-import {Link, useNavigate,useParams} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import appwriteService from "../appwrite/config";
-import { Button,Container } from '../components';
-import parse from 'html-react-parser';
-import { useSelector } from 'react-redux';
-
+import { Button, Container } from "../components";
+import parse from "html-react-parser";
+import { useSelector } from "react-redux";
 
 export default function Post() {
-    const [post, setPost] = useState(null);
-    const {slug} = useParams();
-    const navigate = useNavigate();
+  const [post, setPost] = useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
 
-    const userData = useSelector((state)=>state.auth.userData);
+  const userData = useSelector((state) => state.auth.userData);
 
-    const isAuthor = post && userData ? post.userId===userData.$id:false;
+  const isAuthor = post && userData ? post.userId === userData.$id : false;
 
-    useEffect(()=>{
-        if(slug){
-            appwriteService.getPost(slug).then((post)=>{
-                if(post)setPost(post);
-                else navigate("/");
-            });
-        }else navigate("/");
-    },[slug,navigate]);
+  useEffect(() => {
+    if (slug) {
+      appwriteService.getPost(slug).then((post) => {
+        if (post) setPost(post);
+        else navigate("/");
+      });
+    } else navigate("/");
+  }, [slug, navigate]);
 
-    const deletePost = ()=>{
-        appwriteService.deletePost(post.$id).then((status)=>{
-            if(status){
-                appwriteService.deleteFile(post.featuredimage);
-                navigate("/");
-            }
-        });
-    }
+  const deletePost = () => {
+    appwriteService.deletePost(post.$id).then((status) => {
+      if (status) {
+        appwriteService.deleteFile(post.featuredimage);
+        navigate("/");
+      }
+    });
+  };
 
-    return post?(
+  return post ? (
+    <div className="py-8">
+      <Container>
+        <div className="w-full flex justify-center mb-4 relative  rounded-xl p-2">
+          <img
+            src={appwriteService.getFilePreview(post.featuredimage)}
+            alt={post.title}
+            className="rounded-xl h-3/4 w-1/2"
+          />
 
-        <div className='py-8'>
-            <Container>
-                <div className='w-full flex justify-center mb-4 relative border rounded-xl p-2'>
-                    <img 
-                    src={appwriteService.getFilePreview(post.featuredimage)}
-                    alt={post.title}
-                    className='rounded-xl'
-                    />
-
-                    {isAuthor && (
-                        <div className='absolute right-6 top-6'>
-                            <Link to={`/edit-post/${post.$id}`}>
-                                <Button 
-                                bgColor="bg-green-500"
-                                className='mr-3'>
-                                    Edit
-                                </Button>
-                            </Link>
-                            <Button bgColor='bg-red-500'
-                            onClick = {deletePost}
-                            >
-                                Delete
-                            </Button>
-                        </div>
-                    )}
-
-                </div>
-                <div className='w-full mb-6'>
-                    <h1 className='text-2xl font-bold text-neutral-50'>
-                        {post.title}
-                    </h1>
-                </div>
-                <div className='browser-css text-neutral-50'>{parse(post.content)}</div>
-            </Container>
-
+          {isAuthor && (
+            <div className="absolute right-6 top-6">
+              <Link to={`/edit-post/${post.$id}`}>
+                <Button bgColor="bg-green-500" className="mr-3">
+                  Edit
+                </Button>
+              </Link>
+              <Button bgColor="bg-red-500" onClick={deletePost}>
+                Delete
+              </Button>
+            </div>
+          )}
         </div>
-    ):null;
+        <div className="flex justify-center">
+        <div className="border rounded w-1/2">
+          <div className=" mb-6 px-3">
+            <h1 className="text-2xl font-bold text-neutral-50">{post.title}</h1>
+          </div>
+          <div className="browser-css text-neutral-50 px-3 mb-6">
+            {parse(post.content)} lorem*10
+          </div>
+        </div>
+        </div>
+      </Container>
+    </div>
+  ) : null;
 }
-
